92e4c9a5933995698f13837d35266ad43d1259a0
Javi Fernandez <javierfernandezriv@gmail.com>
2021-03-17 14:00:00 +0100

    Creation and Validation for Users

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ console.log("Comenzando App")
 //Puerto de la app
 const port = process.env.PORT || 4000;
 
+//Habilitar leer los valores de un body
+app.use(express.json());
+
+//Rutas de la app
+app.use('/api/usuarios', require('./routes/usuarios'));
+
 //Arrancar la app
 app.listen(port, '0.0.0.0', () => {
     console.log(`Servidor conectado en el puerto ${port}`)
